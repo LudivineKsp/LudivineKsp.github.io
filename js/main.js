a3e3aaab62ba87fc830112d7ba1ae07494b0eb8c
Ludivine Kasper <kasper.ludivine@orange.fr>
2023-01-28 11:20:37 +0100

    finish skills section and add qualification tabs

--- js/main.js
+++ js/main.js
@@ -35,8 +35,45 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /* ============= ACCORDION SKILLS =============== */
 
+const skillsContent = document.getElementsByClassName('skills_content'),
+      skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills() {
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills_content skills__close'
+    }
+    if(itemClass === 'skills_content skills__close'){
+        this.parentNode.className = 'skills_content skills__open'
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills)
+
+})
 
 /* ============= QUALIFICATION TABS =============== */
+
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tab.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
 
 
 /* ============= SERVICE MODAL =============== */
